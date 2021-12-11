72cd29f3f1f5d519dca7b5bfef985941467e7037
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-12-11 09:50:33 -0600

    created all DataAccess repos

---
+++ PlantFriend/DataAccess/LogRepo.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PlantFriend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantFriend.DataAccess
+{
+    public class LogRepo
+    {
+        string _connectionString;
+        public LogRepo(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("PlantFriend");
+        }
+
+        internal IEnumerable<Log> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"select * from Log";
+            var logs = db.Query<Log>(sql);
+            return logs;
+        }
+    }
+}
---
+++ PlantFriend/DataAccess/PlantRepo.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PlantFriend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantFriend.DataAccess
+{
+    public class PlantRepo
+    {
+        string _connectionString;
+        public PlantRepo(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("PlantFriend");
+        }
+
+        internal IEnumerable<Plant> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"select * from Plant";
+            var plants = db.Query<Plant>(sql);
+            return plants;
+        }
+    }
+}

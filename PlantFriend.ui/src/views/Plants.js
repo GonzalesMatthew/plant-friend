5e30bb8a24be53046f7fdfcc8a50ee8626e33d7e
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-12-20 18:23:43 -0600

    stuff with changes from a broken repo

--- PlantFriend.ui/src/views/Plants.js
+++ PlantFriend.ui/src/views/Plants.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PlantCard from '../components/Cards/PlantCard';
 import getPlants from '../helpers/data/PlantData';
 
 function Plants() {
@@ -10,12 +11,27 @@ function Plants() {
     <>
       <div>
         Plants
+        {/* // search bar */}
+        {/* add plants button */}
       </div>
-      {plants.map((plant, i) => (
-        <div key={i}>
-          {plant.name}
-        </div>
-      ))}
+      <div className='d-flex flex-column justify-content-center align-items-center' >
+        {plants.map((plant, i) => (
+          <PlantCard
+            key={i}
+            id={plant.id}
+            name={plant.name}
+            light={plant.light}
+            water={plant.water}
+            waterFrequency={plant.waterFrequency}
+            temperature={plant.temperature}
+            nutrients={plant.nutrients}
+            nutrientsFrequency={plant.nutrientsFrequency}
+            description={plant.description}
+            careNeeds={plant.careNeeds}
+            imageUrl={plant.imageUrl}
+          />
+        ))}
+      </div>
     </>
   );
 }
